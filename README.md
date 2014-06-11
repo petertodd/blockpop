144c5a7ad8c1aa31896c2f1961a4c46354193b80
Peter Todd <pete@petertodd.org>
2014-06-10 22:59:10 -0400

    framework

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ blockpop
 ========
 
 Proof-of-publication with the blockchain
+
+
+Unit Tests
+==========
+
+python3 -m unittest discover -s blockpop
---
+++ blockpop/test/test_blockpop.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2014 Peter Todd <pete@petertodd.org>
+#
+# This file is part of Blockpop.
+#
+# It is subject to the license terms in the LICENSE file found in the top-level
+# directory of this distribution.
+#
+# No part of Blockpop, including this file, may be copied, modified,
+# propagated, or distributed except according to the terms contained in the
+# LICENSE file.
+
+import unittest
+
+class Test(unittest.TestCase):
+    pass
